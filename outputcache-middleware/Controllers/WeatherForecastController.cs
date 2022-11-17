b507a78afcd42c5dff28cbe96ed66add5e3960f1
Hakan Hidir <hakan.hidir@technicsatelier.com>
2022-11-17 01:57:57 +0100

    init

---
+++ outputcache-middleware/Controllers/WeatherForecastController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.OutputCaching;
+
+namespace outputcache_middleware.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+//[OutputCache]
+public class WeatherForecastController : ControllerBase
+{
+    private static readonly string[] Summaries = new[]
+    {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+    private readonly Dictionary<string, List<string>> _countries;
+    private readonly IOutputCacheStore _cache;
+
+
+    private readonly ILogger<WeatherForecastController> _logger;
+
+    public WeatherForecastController(ILogger<WeatherForecastController> logger, IOutputCacheStore cache)
+    {
+        _logger = logger;
+        this._countries = new Dictionary<string, List<string>>();
+        _countries.Add("Germany", new List<string>() { "Stuttgart", "Munich" });
+        _countries.Add("France", new List<string>() { "Paris", "Lyon" });
+        _cache = cache;
+    }
+
+    [HttpGet(Name = "GetWeatherForecast")]
+    [OutputCache]
+    public IActionResult Get()
+    {
+        return Ok(Summaries);
+    }
+
+    [HttpGet]
+    [Route("City")]
+    //[OutputCache(Duration = 1000, VaryByQueryKeys = new string[] { "country" })]
+    [OutputCache(PolicyName = "Query1")]
+    public IActionResult GetCityByCountry([FromQuery] string country)
+    {
+        var result = _countries[country];
+        return Ok(result);
+    }
+
+    [HttpPost]
+    [Route("CityPurge")]
+    public async Task<IActionResult> CitcCachePurge()
+    {
+        await this._cache.EvictByTagAsync("tag-country", default);
+        return Ok();
+    }
+}
+
---
+++ outputcache-middleware/Program.cs
@@ -0,0 +1,35 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+
+//builder.Services.AddOutputCache();
+builder.Services.AddOutputCache(options =>
+{
+    options.AddBasePolicy(builder => builder.Expire(TimeSpan.FromSeconds(10)));
+    options.AddPolicy("Query1", builder => builder.SetVaryByQuery("country").Expire(TimeSpan.FromSeconds(500)).Tag("tag-country"));
+    options.AddPolicy("Header", builder => builder.SetVaryByHeader("X-MyHeader").Expire(TimeSpan.FromSeconds(1000)));
+    options.AddPolicy("NoCache", builder => builder.NoCache());
+    options.AddPolicy("NoLock", builder => builder.SetLocking(false));
+});
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+app.UseOutputCache();
+app.MapControllers();
+
+app.Run();
+
